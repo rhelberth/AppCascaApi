e0cdaa8a6535db5197e6f1721dfc0cd8384a1e10
Rhelberth <rhelberth@gmail.com>
2025-09-09 13:33:46 -0300

    Use `collectAsStateWithLifecycle` in `GenreScreen`
    The `collectAsStateWithLifecycle` API collects flows in a lifecycle-aware manner, allowing apps to save resources.
    
    The IDE configuration files were also updated.

--- app/src/main/java/com/example/appcascaapi/ui/screens/GenreScreen.kt
+++ app/src/main/java/com/example/appcascaapi/ui/screens/GenreScreen.kt
@@ -11,13 +11,13 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.appcascaapi.ui.theme.AppCascaApiTheme
 import com.example.appcascaapi.ui.viewmodel.GenreViewModel
 
@@ -26,7 +26,7 @@ fun GenreScreen(
     modifier: Modifier = Modifier,
     viewModel: GenreViewModel = hiltViewModel()
 ) {
-    val uiState by viewModel.uiState.collectAsState()
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     Box(
         modifier = modifier.fillMaxSize(),
